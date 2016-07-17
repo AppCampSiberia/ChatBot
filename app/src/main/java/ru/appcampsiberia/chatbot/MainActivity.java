0b10b5380e97164b3651c2d7f86cdeec59f46013
Sashulya Tarantsova <sashulya.tarantsova@gmail.com>
2016-07-17 11:52:52 +0600

    умеет просто писать сообщения

--- app/src/main/java/ru/appcampsiberia/chatbot/MainActivity.java
+++ app/src/main/java/ru/appcampsiberia/chatbot/MainActivity.java
@@ -2,12 +2,44 @@ package ru.appcampsiberia.chatbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText messageField;
+    Editable message;
+    TextView chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chat = (TextView) findViewById(R.id.chat);
+
+        messageField = (EditText) findViewById(R.id.messageField);
+        messageField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                chat.setText(messageField.getText());
+            }
+        });
+
+
+
     }
 }
